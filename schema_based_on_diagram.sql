c5766a87377a9c80f4ce6b627211e7971ad259c4
Cristian Zambrano <cristian@zamcham.com>
2023-06-20 15:33:06 -0400

    create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,12 @@ CREATE TABLE medical_histories (
   status VARCHAR(50),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE invoices (
+  id INT,
+  total_amount DECIMAL(10,2),
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id),
+  PRIMARY KEY(id)
+);
